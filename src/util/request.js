c5d91b44f0b6f609e78e322f49783780b043323a
“guosj” <2547361543@qq.com>
2022-06-10 23:55:31 +0800

    修改端口

--- src/util/request.js
+++ src/util/request.js
@@ -4,7 +4,7 @@ import 'nprogress/nprogress.css'
 import {Message} from 'element-ui'
 
 const request = axios.create({
-	baseURL: 'http://localhost:8090/admin/',
+	baseURL: 'http://121.196.111.86:8090/admin/',
 	timeout: 5000
 })
 
